fda0c186d41b6dba471c8c79a5622e21c558ea10
josh-hayhoe <joshuahayhoe3000@gmail.com>
2024-07-15 17:10:47 +0100

    Initial commit

--- poke_api/settings.py
+++ poke_api/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "pokemon_listing",
-    "rest_framework",
 ]
 
 MIDDLEWARE = [
