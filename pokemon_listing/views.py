43b13f8a3db9badbbd1d81cf2b6e12c6399ce0df
Matthew Moss <mlettis77@gmail.com>
2024-07-16 17:16:33 +0100

    Fetch from pokeAPI and add basic pokemon list to import page

--- pokemon_listing/views.py
+++ pokemon_listing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import requests
+
 # Create our views here.
 
 # create a template (with import button)
@@ -11,4 +13,12 @@ from django.template import loader
 def index(request):
     template = loader.get_template("listing/index.html")
     context = {}
+    return HttpResponse(template.render(context, request))
+
+def fetchPokemon(request):
+    template = loader.get_template("listing/import.html")
+    data = requests.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    context = {
+        "pokemon": data.json()
+    }
     return HttpResponse(template.render(context, request))
