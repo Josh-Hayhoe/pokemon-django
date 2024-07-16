c977323e0a931a6deb681665d0749f233cd3f977
josh-hayhoe <joshuahayhoe3000@gmail.com>
2024-07-16 17:06:35 +0100

    Output the pokemon on the web page and create Name model and migrate to database

--- pokemon_listing/urls.py
+++ pokemon_listing/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from . import views, views as main_views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', main_views.button, name="button"),
+    path('fetch-pokemon/', views.fetch_pokemon, name="fetch_pokemon")
 ]
--- pokemon_listing/views.py
+++ pokemon_listing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import requests
+from pokemon_listing.models import Name
 # Create our views here.
 
 # create a template (with import button)
@@ -8,7 +10,16 @@ from django.template import loader
 # create a URL that the button in template sends request to
 # create method in this view to get the pokemon from the pokeAPI
 
-def index(request):
-    template = loader.get_template("listing/index.html")
+
+def button(request):
+    template = loader.get_template("listing/button.html")
     context = {}
     return HttpResponse(template.render(context, request))
+
+def fetch_pokemon(request):
+    r = requests.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+#for item in r:
+        #name = Name(name=item[0])
+        #name.save()
+        #Able to save to the db in the shell but not here, plus it didnt show up in the physical db anyway
+    return HttpResponse(r)
