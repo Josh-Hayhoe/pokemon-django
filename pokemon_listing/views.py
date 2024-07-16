968e7e231004e2f778ff0d36302ccb2855342001
Matthew Moss <mlettis77@gmail.com>
2024-07-16 15:06:37 +0100

    Create basic template with link to import button

--- pokemon_listing/views.py
+++ pokemon_listing/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
+from django.template import loader
+# import requests
 # Create our views here.
+
+# create a template (with import button)
+# get this view to return the template
+# create a URL that the button in template sends request to
+# create method in this view to get the pokemon from the pokeAPI
+
 def index(request):
-    return HttpResponse(f"Hello world - This is the pokemon listing")
+    template = loader.get_template("listing/index.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
