19b585ba43734c750dbae5e77255f8bc6d2ab4bd
Matthew Moss <mlettis77@gmail.com>
2024-07-17 12:21:02 +0100

    Create Pokemon model and associated migration

--- pokemon_listing/models.py
+++ pokemon_listing/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class Pokemon(models.Model):
+    number = models.IntegerField()
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    sprite_url = models.CharField(max_length=255)
